bb4c07c858bcbb57e2e8863035d3ff3062418f0d
Aidan Moretz <majsigmae@gmail.com>
2022-04-01 18:01:36 -0400

    Refactored InputTable to be more generic

--- MechCustom.py
+++ MechCustom.py
@@ -1,29 +1,44 @@
-from tkinter.ttk import *
+from tkinter.ttk import Frame, Entry, Label, Button
+from typing import Optional
 
-class IOTable(Frame):
-    def __init__(self, titles, units, entries, editible=None, button=None, master=None):
+class InputTable(Frame):
+    def __init__(self, rows: dict[str, list[str, int]], columnTitles: Optional[list[str]]=None, title: str="", button: str="", master=None) -> None:
         Frame.__init__(self,master)
+        self.master.title(title)
         self.grid()
-        self.create_inputs(titles, units, entries, editible, button)
+        self.create_inputs(rows, columnTitles, button)
+        self.mainloop()
 
     #TODO: add editible toggle
-    def create_inputs(self,titles, units, entries, editible, button):
+    def create_inputs(self, rows: dict[str, list[str, int]], columnTitles: Optional[list[str]], button: bool) -> None:
         """Instantiates and lays out the input table fields and buttons"""
-        tableWidth = max(entries)+1
-        # store entry values in dictionary for later
+        titleWidth = max(7, len(button))
+        tableWidth = 0
+        for title, [unit, num] in rows.items():
+            titleWidth = max(titleWidth, len(title)+len(unit)+4)
+            tableWidth = max(tableWidth, num)
+        if columnTitles is None:
+            columnWidth = 7
+            columnTitles = range(1,tableWidth+1)
+        else:
+            columnWidth = max([len(title) for title in columnTitles])
+        # store entry values in dictionary for user access
         self.values = {}
         # title row
-        for column in range(1,tableWidth):
-            Label(self, width=7, text=str(column)).grid(row=0, column=column)
+        for i, column in enumerate(columnTitles):
+            Label(self, width=columnWidth, text=column).grid(row=0, column=i+1)
         # main body
-        for row, title, unit, num in zip(range(1,len(titles)+1), titles, units, entries):
+        for row, (title, [unit, num]) in enumerate(rows.items()):
             # title entry
-            Label(self, width = 15, text=" ".join([title,"[",unit,"]"])).grid(row=row, column=0)
+            Label(self, width=titleWidth, text=f"{title} [{unit}]").grid(row=row+1, column=0)
             # value entries
             self.values[title] = []
             for column in range(1,num+1):
-                self.values[title].append(Entry(self, width=7))
-                self.values[title][-1].grid(row=row, column=column)
+                self.values[title].append(Entry(self, width=columnWidth))
+                self.values[title][-1].grid(row=row+1, column=column)
         # button to close
-        if button is not None:
-            Button(self, width=7, text=button, command=lambda: self.quit()).grid(row=row+1, column=tableWidth-1)
+        if button:
+            Button(self, width=columnWidth, text=button, command=lambda: self.quit()).grid(row=row+2, column=tableWidth)
+
+
+# This will be deleted without the cache
